624fe8daea23b84712c37874149803a46b78e643
Gabriel Emmanuel P. Domondon <gabemmanuel468@gmail.com>
2026-08-13 09:08:42 +0800

    Update script.js

--- debugging-exercise/script.js
+++ debugging-exercise/script.js
@@ -11,18 +11,15 @@ function displayGreeting() {
     const userName = nameInput.value.trim();
 
     if (userName == "") {
-        resultMessage.textContent =
-            "Please enter your name.";
+        resultMessage.textContent = "Please enter your name.";
     } else {
         heading.textContent = "Hello, " + userName;
-        resultMessage.textContent =
-            "The greeting was displayed successfully."
+        resultMessage.textContent = "The greeting was displayed successfully."
     }
 }
 
 function showTypedText() {
-    typingMessage.textContent =
-        "You are typing: " + nameInput.value;
+    typingMessage.textContent = "You are typing: " + nameInput.value;
 }
 
 function changeBackground() {
@@ -34,8 +31,7 @@ function resetPage() {
     heading.textContent = "Event-Driven Webpage";
     nameInput.value = "";
     typingMessage.textContent = "You are typing:";
-    resultMessage.textContent =
-        "Enter your name and select an action.";
+    resultMessage.textContent = "Enter your name and select an action.";
     document.body.style.backgroundColor = "#f3f4f6";
 }
 
